60434a8eb6c8df5634ef44ad4be37c50a87db244
sabflik <szaf045@aucklanduni.ac.nz>
2015-09-26 16:31:10 +1200

    Initial files

---
+++ Helper.java
@@ -0,0 +1,30 @@
+package gui;
+
+import javax.swing.SwingWorker;
+
+public class Helper extends SwingWorker<Void, Void>{ //this class calls festival in the background for the speech button in the main frame
+
+	private String speech;
+	private Process process;
+	
+	public Helper(String speech) {
+		this.speech = speech.replace("\n", " "); //if new lines have been entered these are replaced with spaces so festival will run
+	}
+	
+	@Override
+	protected Void doInBackground() throws Exception { //call festival
+		
+		String cmd = "echo "+speech+" | festival --tts"; 
+		
+		ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
+		
+		try {
+			process = builder.start();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		return null;
+	}
+
+}
